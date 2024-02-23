73d4e27c7ee9601796332fc4a36526ca1f8043f9
sachin.kumar <sachin.kumar.maheshwarappa@gmail.com>
2024-02-23 11:46:04 +0530

    Fetch data from api and load it in the UI

---
+++ src/Components/Nav.js
@@ -0,0 +1,32 @@
+
+import {NavLink} from 'react-router-dom'
+
+const navStyle  = {
+    "display": "flex",
+    "justifyContent": "flex-end",
+    "backgroung": "",
+    "height": "3rem",
+    "alignItems": "center",
+    "background":"#d1d1e3"
+  }
+  
+  const margins = {
+      "margin": "1px 15px 0px 0px",
+      "cursor": "pointer"
+  }
+
+function NavBar() {
+    return (
+        <div style={navStyle}>
+           
+            <div style={margins}>
+                <NavLink to="/">Home</NavLink>
+            </div>
+            <div style={margins}>
+                <NavLink to="/cart">Cart</NavLink>
+            </div >
+            <div style={margins}>Cart Items 0</div>
+        </div>
+    )
+}
+export default NavBar
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,48 @@
+import { useState, useEffect } from "react"
+import { URL } from "/Users/sacmahes/Desktop/My Projects/React and Redux practice/redux/src/constants.js"
+import axios from 'axios'
 
-
+const imageStyle = {
+    "background-repeat": "no-repeat",
+    "background-size": "cover",
+}
 function Home() {
+    const [products, setProducts] = useState([])
+    const [loading, setIsLoading] = useState(true)
+    async function getProducts() {
+        setIsLoading(true)
+        const response = await axios.get(URL)
+        if (response.status === 200) {
+            setProducts(response.data)
+            setIsLoading(false)
+        }
+        
+    }
+    useEffect(() => {
+        getProducts()
+    }, [])
+
+    if (loading) {
+        return <div className="loading">
+        <h2>Loading.....</h2>
+        </div>
+    }
     return (
-        <div>
-            This is Home
+        
+        <div style={{ display: "flex", alignItems: "center", flexWrap: "wrap", gap: 45, marginTop: 20, justifyContent: "center", paddingBottom: 30 }}>
+            {products.map((product) => {
+                const { title, price, image, id } = product
+                return <div className="products" key={id} style={{width: 200, height: 400}}>
+                    <div style={{ margin:"auto",width: 190, height: 300, background: `url(${image})`,display:"flex",alignContent:"center", backgroundRepeat: 'no-repeat', backgroundSize: 'auto', WebkitBackgroundSize: "contain", flexDirection: "column-reverse", backgroundPosition:"center" }}>
+                    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column-end", }}> 
+                    </div>
+                    </div>
+                    <div style={{height:40, overflow:"hidden"}}>{title}</div>
+                    <div style={{ fontWeight: "bold" }}>₹ {price}</div>
+                    <button className="btn">Add to Cart</button>
+                </div>
+            })}
+            
         </div>
     )
 }
