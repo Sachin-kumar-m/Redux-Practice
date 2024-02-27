688cbfa73ec20aa14830152bae0f8791a770bf51
sachin.kumar <sachin.kumar.maheshwarappa@gmail.com>
2024-02-27 22:37:52 +0530

    subscribed the product slice in store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import Cart from "./Slices/CartSlice";
+import Product from "./Slices/ProductSlice"
 
 export const store = configureStore({
     reducer: {
         Cart,
+        Product,
     }
 })
